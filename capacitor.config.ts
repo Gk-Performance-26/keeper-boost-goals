2e9e9cb24029ad768487e0f33627b61b1c0242f2
Guilherme Fernandes <guisantos0621@MacBook-Pro-de-Guilherme.local>
2026-04-29 18:33:29 +0100

    setup ios with capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.guilherme.gkperformancehub',
+  appName: 'GKPerformanceHub',
+  webDir: 'dist'
+};
+
+export default config;
